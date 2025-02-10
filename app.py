61f39a862542c291689793ac5bc47667691f4992
guilanzaschmidt <65963917+guilanzaschmidt@users.noreply.github.com>
2025-02-10 23:44:30 +0000

    Segunda Parte

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ from datetime import datetime as dt
 from streamlit_extras.metric_cards import style_metric_cards
 from streamlit_extras.grid import grid
 
-st.title("Analise ações brasileiras")
-
 def build_sidebar():
     st.image("Images/logo-250-100-transparente.png")
     ticket_list = pd.read_csv("tickers_ibra.csv", index_col=0)
@@ -18,16 +16,55 @@ def build_sidebar():
     #start_date = tz.localize(dt(2024,1,1))
     #end_date = tz.localize(dt.today())
     start_date = st.date_input("De", format="DD/MM/YYYY",value=dt(2024,1,2))
-    end_date = st.date_input("Até", format="DD/MM/YYYY",value=dt(2025,1,13))
+    end_date = st.date_input("Até", format="DD/MM/YYYY",value="today")
 
     if tickers:
         prices = yf.download(tickers, start = start_date, end = end_date, auto_adjust=True)["Close"]
+        prices.columns = prices.columns.str.rstrip(".SA")
         return tickers, prices
+    return None, None
 
 def build_main(tickers, prices):
-    st.dataframe(prices)
+    weigths = np.ones(len(tickers))/len(tickers)
+    prices['portfolio'] = prices @ weigths
+    norm_prices = 100 * prices / prices.iloc[0]
+    returns = prices.pct_change()[1:]
+    vols = returns.std()*np.sqrt(252)
+    rets = (norm_prices.iloc[-1] - 100) / 100
+
+    mygrid = grid(5 ,5 , 5 ,5 ,5 ,5, vertical_align="top")
+    for t in prices.columns:
+        c = mygrid.container(border=True)
+        c.subheader(t, divider="red")
+        colA, colB, colC, = c.columns(3)
+        if t == "portfolio":
+            colA.image("Images/pie-chart-dollar-svgrepo-com.svg")   
+        else:     
+            colA.image(f'https://raw.githubusercontent.com/thefintz/icones-b3/main/icones/{t}.png', width=85)    
+        colB.metric(label="retorno", value=f"{rets[t]:.0%}")
+        colC.metric(label="volatidade", value=f"{vols[t]:.0%}")
+        style_metric_cards(background_color='rgba(255,255,255,0)')
+
+    col1, col2 = st.columns(2, gap='large')
+    with col1:
+        st.subheader("Desempenho Relativo")
+        st.line_chart(norm_prices, height=600)
+
+    with col2:
+        st.subheader("RiscoxRetorno")
+        fig = px.scatter(
+            x=vols,
+            y=rets,
+            color=rets/vols,
+            color_continuous_scale=px.colors.sequential.Bluered_r
+        )       
+    #st.dataframe(prices)
+
+st.set_page_config(layout="wide")    
 
 with st.sidebar:
     tickers, prices = build_sidebar()
 
-build_main(tickers, prices)
+if tickers:
+    build_main(tickers, prices)
+st.title("Analise ações brasileiras")
